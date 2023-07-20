1040cd960776881593375842f80a1d74edd5b4c4
JosFer720 <119770216+JosFer720@users.noreply.github.com>
2023-07-20 16:25:36 -0600

    Changes to Marketworld
    Added a syringe, modified the spawn method, and implemented movement towards the nearest syringe.

---
+++ survid-19/Syringe.java
@@ -0,0 +1,24 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Syringe here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Syringe extends Actor
+{
+    /**
+     * Act - do whatever the Syringe wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public Syringe(){
+        GreenfootImage image = getImage();
+        image.scale(40,40);
+        setImage(image);
+    }
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
