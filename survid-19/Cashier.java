069a95f7d2ec1577a139a587c8732fd6bc11c07c
JosFer720 <119770216+JosFer720@users.noreply.github.com>
2023-07-21 17:26:35 -0600

    Objects

--- survid-19/Cashier.java
+++ survid-19/Cashier.java
@@ -12,8 +12,7 @@ public class Cashier extends Actor
      * Act - do whatever the Cashier wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public void act()
-    {
-        // Add your action code here.
+    public void act(){
+
     }
 }
--- survid-19/Marketworld.java
+++ survid-19/Marketworld.java
@@ -20,6 +20,14 @@ public class Marketworld extends World
         setp1();
         randomEnemies();
         randomSyringe();
+        SetShelves();
+        SetNorthL();
+        SetSouthL();
+        SetEastL();
+        SetWestL();
+        SetFruits();
+        SetDrinks();
+        SetCashier();
     }
     
     public void setp1(){
@@ -46,8 +54,6 @@ public class Marketworld extends World
         }
     }
     
-   
-    
     private void addRandomObject(Actor object, Random rand) {
         int x, y;
         do {
@@ -57,6 +63,102 @@ public class Marketworld extends World
         
         addObject(object, x, y);
     }
+
+    public void SetShelves(){
+        int[][] shelveCoordinates = {
+            {110, 173},
+            {166, 173},
+            {222, 173},
+            {278, 173},
+            {334, 173},
+            {520, 173},
+            {575, 173}
+        };
+        
+        for (int i = 0; i < shelveCoordinates.length; i++) {
+            Shelve s = new Shelve();
+            addObject(s, shelveCoordinates[i][0], shelveCoordinates[i][1]);
+        }
+    }
+    
+    public void SetNorthL(){
+        int[][] NorthLCoordinates = {
+            {204, 70},
+            {600, 70},
+        };
+        
+        for (int i = 0; i < NorthLCoordinates.length; i++) {
+            NorthL NL = new NorthL();
+            addObject(NL, NorthLCoordinates[i][0], NorthLCoordinates[i][1]);
+        }
+    }
+    
+    public void SetSouthL(){
+        int[][] SouthLCoordinates = {
+            {204, 400},
+            {600, 400},
+        };
+        
+        for (int i = 0; i < SouthLCoordinates.length; i++) {
+            SouthL SL = new SouthL();
+            addObject(SL, SouthLCoordinates[i][0], SouthLCoordinates[i][1]);
+        }
+    }
+    
+    public void SetEastL(){
+        EastL E1 = new EastL();
+        addObject(E1, 600, 204);
+    }
+    
+    public void SetWestL(){
+        WestL E1 = new WestL();
+        addObject(E1, 0, 247);
+    }
+    
+    public void SetFruits(){
+        int[][] FruitsCoordinates = {
+            {49, 185},
+            {49, 232},
+            {457, 185},
+            {457, 232},
+        };
+        
+        for (int i = 0; i < FruitsCoordinates.length; i++) {
+            Fruits F = new Fruits();
+            addObject(F, FruitsCoordinates[i][0], FruitsCoordinates[i][1]);
+        }
+    }
+    
+    public void SetDrinks(){
+        int[][] DrinksCoordinates = {
+            {49, 296},
+            {49, 351},
+            {121, 296},
+            {121, 351},
+            {456, 296},
+            {456, 351},
+            {529, 296},
+            {529, 351},
+        };
+        
+        for (int i = 0; i < DrinksCoordinates.length; i++) {
+            Drinks D = new Drinks();
+            addObject(D, DrinksCoordinates[i][0], DrinksCoordinates[i][1]);
+        }
+    }
+    
+    public void SetCashier(){
+        int[][] CashierCoordinates = {
+            {188, 327},
+            {235, 327},
+            {284, 327},
+            {332, 327},
+            {589, 327}
+        };
+        
+        for (int i = 0; i < CashierCoordinates.length; i++) {
+            Cashier C = new Cashier();
+            addObject(C, CashierCoordinates[i][0], CashierCoordinates[i][1]);
+        }
+    }
 }
-
-
